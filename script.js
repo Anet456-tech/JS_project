1ba6a021b04f72e1dcfdb12a71752c90bc199341
Asna M S <asnams625@gmail.com>
2025-11-08 17:59:25 +0530

    fix: correct variable name in shuffleWord function

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const levelLabel = document.getElementById("level-label");
 // ===== SHUFFLE WORD FUNCTION =====
 function shuffleWord(word) {
   let arr = word.split("");
-  for (let i = lastIndex; i > 0; i--) {
+  for (let i =letters.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
